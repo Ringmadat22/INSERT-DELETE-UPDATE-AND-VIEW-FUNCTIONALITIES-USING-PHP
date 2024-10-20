a6a21be552324c20646f6b4f4cae3e8efc82bd10
Ring Madat <smithemma329@gmail.com>
2024-10-20 14:39:40 +0300

    main

--- view.php
+++ view.php
@@ -24,30 +24,43 @@
     </style>
     <?php
         include "connection.php";
-        
-        $sql = "SELECT * FROM users";
+
+        $sql = "SELECT * FROM users WHERE 1=1";
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+
+            if(!empty($username)){
+                $sql .= " AND username like '%$username%'";
+            }
+
+            if(!empty($email)) {
+                $sql .= " AND email like '%$email%'";
+            }
+        }
+
         $result = $conn->query($sql);
 
         if($result->num_rows > 0) {
             //DISPLAY DATA INFORM OF A TABLE
 
             echo "<table>";
-            echo "<tr><th>ID</th><th>Username</th><th>Email</th></tr>";
-
-            while($row = $result->fetch_assoc()){
+            echo "<tr><th>ID</th><th>USERNAME</th><th>EMAIL</th></tr>";
+            
+            while($row = $result->fetch_assoc()) {
                 echo "<tr>";
-                echo "<td>" . $row["id"] . "</td>";
-                echo "<td>" . $row["username"] . "</td>";
-                echo "<td>" . $row["email"] . "</td>";
+                echo "<td>" . $row['id'] . "</td>";
+                echo "<td>" . $row['username'] . "</td>";
+                echo "<td>" . $row['email'] . "</td>";
             }
+
         } else {
             echo "No records";
         }
 
         $conn->close();
-        
     ?>
-
     <form action="view.php" method="POST" >
         username: <input type="text" name="username" placeholder="Username">
         email: <input type="email" name="email" placeholder="Email">
