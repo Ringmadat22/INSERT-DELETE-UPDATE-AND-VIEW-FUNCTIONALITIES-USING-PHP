315345f7be7a83771d8add3777380bd3e83c6fbe
Ring Madat <smithemma329@gmail.com>
2024-10-20 14:19:49 +0300

    main

---
+++ view.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VIEW</title>
+</head>
+<body>
+    <style>
+         table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 10px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+    <?php
+        include "connection.php";
+        
+        $sql = "SELECT * FROM users";
+        $result = $conn->query($sql);
+
+        if($result->num_rows > 0) {
+            //DISPLAY DATA INFORM OF A TABLE
+
+            echo "<table>";
+            echo "<tr><th>ID</th><th>Username</th><th>Email</th></tr>";
+
+            while($row = $result->fetch_assoc()){
+                echo "<tr>";
+                echo "<td>" . $row["id"] . "</td>";
+                echo "<td>" . $row["username"] . "</td>";
+                echo "<td>" . $row["email"] . "</td>";
+            }
+        } else {
+            echo "No records";
+        }
+
+        $conn->close();
+        
+    ?>
+
+    <form action="view.php" method="POST" >
+        username: <input type="text" name="username" placeholder="Username">
+        email: <input type="email" name="email" placeholder="Email">
+        <input type="submit" value="Filter" >
+    </form>
+</body>
+</html>
